91e44d867832d5079107dce06f713b1cff01521a
Nidhi <nidhipatel439@gmail.com>
2022-02-11 01:32:07 -0500

    modify delete nd add button

--- src/Todo.js
+++ src/Todo.js
@@ -3,9 +3,31 @@ import TextField from "@mui/material/TextField";
 import FormGroup from "@mui/material/FormGroup";
 import FormControlLabel from "@mui/material/FormControlLabel";
 import Checkbox from "@mui/material/Checkbox";
-import { Card, CardContent, CardHeader, Grid, Typography } from "@mui/material";
+import { Card, CardContent, CardHeader, Grid } from "@mui/material";
 
 function Todo() {
+  const [itemList, setItemList] = React.useState([]);
+
+  function addItem() {
+    const item = document.getElementById("outlined-basic").value;
+    // console.log(itemList);
+    // const prevItems = [...itemList]
+    setItemList([...itemList, item]);
+
+    document.getElementById("outlined-basic").value = "";
+  }
+
+  function deleteItem(indexOfList) {
+    const newList = itemList.filter((item, index) => index !== indexOfList);
+    setItemList(newList);
+  }
+
+  function onEnter(e) {
+    if (e.keyCode === 13) {
+      addItem();
+    }
+  }
+
   return (
     <Grid
       container
@@ -19,17 +41,31 @@ function Todo() {
             title="To Do List"
             titleTypographyProps={{ variant: "h2", component: "h3" }}
           />
+
           <CardContent>
             <TextField
               id="outlined-basic"
               label="Outlined"
               variant="outlined"
               fullWidth
+              onKeyUp={onEnter}
             />
+            <button onClick={addItem}>Add</button>
             <FormGroup>
-              <FormControlLabel control={<Checkbox />} label="Milk" />
-              <FormControlLabel control={<Checkbox />} label="Sugar" />
-              <FormControlLabel control={<Checkbox />} label="Eggs" />
+              {itemList.map((x, y) => {
+                return (
+                  <div key={y}>
+                    <FormControlLabel control={<Checkbox />} label={x} />
+                    <button
+                      onClick={function (event) {
+                        deleteItem(y);
+                      }}
+                    >
+                      delete
+                    </button>
+                  </div>
+                );
+              })}
             </FormGroup>
           </CardContent>
         </Card>
